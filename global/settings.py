312e049073f35a8d64b4f8f6ac7845d8e74a6397
Muhiddin1408 <81641677+Muhiddin1408@users.noreply.github.com>
2022-01-11 10:34:33 +0500

    CATEGORY

--- global/settings.py
+++ global/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bloc.apps.BlocConfig',
-
+    'adminlte3',
+    'adminlte3_theme',
     'materializecssform',
 ]
 
