b165ce68ac980dc23b3d7d74563e506eb6a6e87b
Muhiddin1408 <81641677+Muhiddin1408@users.noreply.github.com>
2022-01-14 10:15:58 +0500

    CATEGORY

--- global/urls.py
+++ global/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('bloc.urls')),
-    path('dashboard', include('dashboard.urls')),
+    path('dashboard/', include('dashboard.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
 
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
